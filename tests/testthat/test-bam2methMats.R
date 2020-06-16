80ccababa7ee051de1f6d2d413d73f3e29abd9cd
jsemple19 <semple.j@gmail.com>
2020-06-16 13:34:17 +0200

    add Xchr data

--- tests/testthat/test-bam2methMats.R
+++ tests/testthat/test-bam2methMats.R
@@ -4,9 +4,9 @@ context("Convert bam files to methylation matrices")
 testthat::test_that("getting mpileup from bam works", {
   bamfile<-system.file("extdata", "aln/dS03-N2_20181119.noOL.bam",
                        package="methMatrix", mustWork=TRUE)
-  genomefile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic.fa",
+  genomefile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic_Xchr.fa",
                           package="methMatrix", mustWork=TRUE)
-  bedfile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic.CG.bed",
+  bedfile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic_Xchr.CG.bed",
                        package="methMatrix", mustWork=TRUE)
   testthat::expect_equal()
 })
