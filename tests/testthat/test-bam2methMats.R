16963ba79fdab0a1950a265ed886a010b42f8768
jsemple19 <semple.j@gmail.com>
2020-06-16 12:20:03 +0200

    changing gitignore

--- tests/testthat/test-bam2methMats.R
+++ tests/testthat/test-bam2methMats.R
@@ -1,8 +1,12 @@
 context("Convert bam files to methylation matrices")
 
 
-# testthat::test_that("getting mpileup from bam works", {
-#   fn<-system.file("extdata", "aln/dS03-N2_20181119.noOL.bam", package="methMatrix", mustWork=TRUE)
-#
-#   testthat::expect_equal()
-# })
+testthat::test_that("getting mpileup from bam works", {
+  bamfile<-system.file("extdata", "aln/dS03-N2_20181119.noOL.bam",
+                       package="methMatrix", mustWork=TRUE)
+  genomefile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic.fa",
+                          package="methMatrix", mustWork=TRUE)
+  bedfile<-system.file("extdata", "c_elegans.PRJNA13758.WS250.genomic.CG.bed",
+                       package="methMatrix", mustWork=TRUE)
+  testthat::expect_equal()
+})
