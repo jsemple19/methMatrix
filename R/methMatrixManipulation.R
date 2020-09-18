7b1bbaf372b4f75095b8304170ddc7130c060389
jsemple19 <semple.j@gmail.com>
2020-09-18 14:49:00 +0200

    fixing single molecule black background plots and adding paramters for nanopore plotting: scaleFactor, midpoint.Making clustring optional.

--- R/methMatrixManipulation.R
+++ R/methMatrixManipulation.R
@@ -212,7 +212,8 @@ plotSingleMolecules<-function(mat,regionName, regionGRs, featureGRs=NULL,
                               baseFontSize=12, maxNAfraction=0.2, segmentSize=3,
                               colourChoice=list(low="blue", mid="white",
                                                 high="red", bg="white",
-                                                lines="black")) {
+                                                lines="black"),
+                              colourScaleMidpoint=0.5, doClustering=T) {
   position<-methylation<-molecules<-NULL
   ### single molecule plot. mat is matrix containing methylation values at different postions
   # (columns) in individual reads (rows). regionName is the ID of the amplicon or genomic
@@ -235,9 +236,9 @@ plotSingleMolecules<-function(mat,regionName, regionGRs, featureGRs=NULL,
       stats::hclust(stats::dist(apply(mat,2,as.numeric))),
       silent = TRUE)
     mat[na.matrix]<-NA
-    if (class(hc) == "try-error") {
+    if (class(hc) == "try-error" | !doClustering ) {
       df<-as.data.frame(mat,stringsAsFactors=F)
-      print("hclust failed. Matrix dim: ")
+      print("hclust not performed. Matrix dim: ")
       print(dim(mat))
     } else {
       df<-as.data.frame(mat[hc$order,], stringsAsFactors=F)
@@ -258,20 +259,25 @@ plotSingleMolecules<-function(mat,regionName, regionGRs, featureGRs=NULL,
       title=paste0(regionName, ": ", GenomicRanges::seqnames(regionGR),
                    " ", strandInfo)
     }
-    p<-ggplot2::ggplot(d,ggplot2::aes(x=position,y=molecules,width=2)) +
-      ggplot2::geom_tile(ggplot2::aes(width=segmentSize, fill=methylation),
+
+    scaleFactor<-(width(regionGR)/500)
+
+    p<-ggplot2::ggplot(d,ggplot2::aes(x=position,y=molecules)) +
+      ggplot2::geom_tile(ggplot2::aes(width=segmentSize*scaleFactor,
+                                      fill=methylation),
                          alpha=0.8) +
       ggplot2::scale_fill_gradient2(low=colourChoice$low, mid=colourChoice$mid,
                                    high=colourChoice$high,
-                                   na.value="transparent",
+                                   midpoint=colourScaleMidpoint,
+                                   na.value=colourChoice$bg,
                                    breaks=c(0,1),
                                    labels=c("protected","accessible"),
                                    limits=c(0,1), name="dSMF\n\n") +
       #ggplot2::scale_fill_manual(values=c("0"="black","1"="grey80"),na.translate=F,na.value="white", labels=c("protected","accessible"),name="dSMF") +
       ggplot2::theme_light(base_size=baseFontSize) +
       ggplot2::theme(panel.grid.major = ggplot2::element_blank(),
-                     panel.grid.minor = ggplot2::element_blank(),
-                     panel.background=ggplot2::element_rect(fill=colourChoice$bg),
+            panel.grid.minor = ggplot2::element_blank(),
+            panel.background=ggplot2::element_rect(fill=colourChoice$bg),
             plot.title = ggplot2::element_text(face = "bold",hjust = 0.5),
             legend.position="bottom",
             legend.key.height = ggplot2::unit(0.2, "cm"),
@@ -281,24 +287,24 @@ plotSingleMolecules<-function(mat,regionName, regionGRs, featureGRs=NULL,
       ggplot2::ylab("Single molecules") +
       ggplot2::xlim(GenomicRanges::start(regionGR),
                     GenomicRanges::end(regionGR)+10)
-    if(length(featureGRs)>0) {
+    if(!is.null(featureGRs)) {
       p<-p+ggplot2::geom_linerange(ggplot2::aes(
         x=GenomicRanges::start(featGR), y=NULL, ymin=0,
         ymax=length(reads)+max(3,0.04*length(reads))),
         col=colourChoice$lines) +
         ggplot2::annotate(geom="text", x=GenomicRanges::start(featGR),
                           y=-max(2,0.03*length(reads)),
-                          label=featureLabel,color="black")
+                          label=featureLabel,color=colourChoice$lines)
       if (drawArrow==TRUE) {
         p<-p+ggplot2::annotate("segment", x = GenomicRanges::start(featGR),
-                          xend = GenomicRanges::start(featGR)+
-                            20*ifelse(GenomicRanges::strand(featGR)=="-",
+                        xend = GenomicRanges::start(featGR)+
+                        20*scaleFactor*ifelse(GenomicRanges::strand(featGR)=="-",
                                       -1,1),
-                          y = length(reads)+max(3,0.04*length(reads)),
-                          yend =length(reads)+max(3,0.04*length(reads)),
-                          colour = colourChoice$lines,
-                          arrow=ggplot2::arrow(length = ggplot2::unit(0.3,
-                                                        "cm")), size=0.7)
+                        y = length(reads)+max(3,0.04*length(reads)),
+                        yend =length(reads)+max(3,0.04*length(reads)),
+                        colour = colourChoice$lines,
+                        arrow=ggplot2::arrow(length = ggplot2::unit(0.3,"cm")),
+                        size=0.7)
       }
     }
   } else {
@@ -337,7 +343,8 @@ plotSingleMoleculesWithAvr<-function(mat, regionName, regionGRs, featureGRs,
                                      maxNAfraction=0.2, segmentSize=3,
                                      colourChoice=list(low="blue", mid="white",
                                                        high="red", bg="white",
-                                                       lines="grey80")) {
+                                                       lines="grey80"),
+                                     colourScaleMidpoint=0.5, doClustering=T) {
   dSMF<-molecules<-position<-methylation<-NULL
   # remove reads with more than maxNAfraction positions with NAs
   tooManyNAs<-rowMeans(is.na(mat))>maxNAfraction
@@ -354,7 +361,7 @@ plotSingleMoleculesWithAvr<-function(mat, regionName, regionGRs, featureGRs,
       stats::hclust(stats::dist(apply(mat,2,as.numeric))),
       silent = TRUE)
     mat[na.matrix]<-NA
-    if (class(hc) == "try-error") {
+    if (class(hc) == "try-error" | !doClustering ) {
       df<-as.data.frame(mat,stringsAsFactors=F)
       print("hclust failed. Matrix dim: ")
       print(dim(mat))
@@ -383,10 +390,11 @@ plotSingleMoleculesWithAvr<-function(mat, regionName, regionGRs, featureGRs,
       ggplot2::theme_light(base_size=baseFontSize) +
       ggplot2::ylab("Mean dSMF") +
       ggplot2::theme(axis.title.x = ggplot2::element_blank(),
-                     axis.text.x = ggplot2::element_blank()) +
+                     axis.text.x = ggplot2::element_blank(),
+                     plot.background = ggplot2::element_rect(colour=colourChoice$bg)) +
       ggplot2::ylim(0,1) +
       ggplot2::xlim(GenomicRanges::start(regionGR),GenomicRanges::end(regionGR)+20)
-    if (length(featureGRs)>0) { # plot feature if present
+    if (!is.null(featureGRs)) { # plot feature if present
       p1<-p1 + ggplot2::geom_linerange(ggplot2::aes(x=GenomicRanges::start(featGR),
                                                     y=NULL, ymin=0, ymax=1),
                                        col=colourChoice$lines,size=0.7)
@@ -402,7 +410,8 @@ plotSingleMoleculesWithAvr<-function(mat, regionName, regionGRs, featureGRs,
     p2<-ggplot2::ggplot(d,ggplot2::aes(x=position,y=molecules,width=2)) +
       ggplot2::geom_tile(ggplot2::aes(width=segmentSize,fill=methylation),alpha=0.8) +
       ggplot2::scale_fill_gradient2(low=colourChoice$low, mid=colourChoice$mid,
-                                    high=colourChoice$high, na.value="transparent",
+                                    high=colourChoice$high, midpoint=colourScaleMidpoint,
+                                    na.value="transparent",
                                    breaks=c(0,1), labels=c("protected","accessible"),
                                    limits=c(0,1), name="dSMF\n\n") +
       ggplot2::theme_light(base_size=baseFontSize) +
